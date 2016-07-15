1968111ed44ce161aa1a1ec79e365920ef547cb6
wangzhongshan <1650043271@qq.com>
2016-07-15 14:50:58 +0800

    todo

---
+++ demo-javase/src/main/java/pers/wzsframework/demo/javase/number/NumberTest.java
@@ -0,0 +1,14 @@
+package pers.wzsframework.demo.javase.number;
+
+/**
+ * @author wangzhongshan
+ * @created 2016-07-15 14:36
+ */
+public class NumberTest {
+    public static void main(String[] args) {
+        float d1=314.3f;
+        float d2=72;
+        System.out.println("==============="+d1*d2);
+
+    }
+}
